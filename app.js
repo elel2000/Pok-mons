cb7c38b06a66a4bab5af5fa93ef22b4371c8fe4e
unknown <hamza.elalamy1@uit.ac.ma>
2024-02-02 12:52:05 +0100

    getting first pokemon

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const fetchPoke = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/1`;
     fetch(url)
         .then(response => {return response.json();})
-        .then(data =>  {console.log(data);})
+        .then(data =>  
+            {console.log(data);
+            const pokemon = {
+                id : data.id,
+                name : data.name,
+                image : data.sprites["front_default"] ,
+                type : data.types.map((type) => type.type.name).join(", ") ,
+                };
+            console.log(pokemon)})
 }
 
 fetchPoke();
