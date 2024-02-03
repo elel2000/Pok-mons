10250bfde10ad8b83f6a4369aec32cc9bc283c5c
unknown <hamza.elalamy1@uit.ac.ma>
2024-02-03 10:58:13 +0100

    using promise.all

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const fetchPoke = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-    fetch(url)
-        .then(response => {return response.json();})
-        .then(data =>  
-            {console.log(data);
-            const pokemon = {
+    const promises = [];
+    for(let i = 1 ; i <= 150 ; i++)
+        {
+            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+            promises.push(fetch(url).then((resp) =>  resp.json()));
+        }
+        Promise.all(promises) .then((results) => { const pokemon = results.map((data) => ({
+             
                 id : data.id,
                 name : data.name,
                 image : data.sprites["front_default"] ,
                 type : data.types.map((type) => type.type.name).join(", ") ,
-                };
-            console.log(pokemon)})
-}
+                
+        }))
+            displayPoke(pokemon);
+    });
+};
 
+function displayPoke(poke){
+    console.log(poke);
+}
 fetchPoke();
